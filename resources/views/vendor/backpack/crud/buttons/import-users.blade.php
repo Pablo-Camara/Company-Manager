6ddc952aaf77deb64e6f032bb25560315b930c8d
Pablo Câmara <pablo.camara.pt@gmail.com>
2022-11-27 20:54:39 +0000

    small mobile fix ( spacing between btns )

--- resources/views/vendor/backpack/crud/buttons/import-users.blade.php
+++ resources/views/vendor/backpack/crud/buttons/import-users.blade.php
@@ -1,3 +1,3 @@
 @if ($crud->hasAccess('create'))
-	<a href="{{ url($crud->route.'/import') }}" class="btn btn-primary" data-style="zoom-in"><span class="ladda-label"><i class="la la-upload"></i> {{ __('Import') }} {{ $crud->entity_name_plural }}</span></a>
+	<a href="{{ url($crud->route.'/import') }}" class="btn btn-primary mt-1 mt-md-0" data-style="zoom-in"><span class="ladda-label"><i class="la la-upload"></i> {{ __('Import') }} {{ $crud->entity_name_plural }}</span></a>
 @endif
